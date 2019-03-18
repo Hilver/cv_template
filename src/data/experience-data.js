9026d16abbd16cc18324c273740c11874f24195e
Pawel <peczkowskip@gmail.com>
2019-03-18 14:55:57 +0100

    An example data has been prepared

--- src/data/experience-data.js
+++ src/data/experience-data.js
@@ -1,34 +1,42 @@
 export const experienceData = {
 	data1: {
-		title: 'CEO',
-		date: '2006/10/20 - 2020/11/03',
-		summary: 'Yolo worker.',
+		title: 'Microsoft',
+		date: '01/2020 - will see',
+		summary: 'Next CEO:',
 		stack: false,
-		skills: [
-			{
-				stack: 'under-construction',
-				icon: 'dupa'
-			}
-		],
-		duties: ['drink coffee', 'make money'],
+		duties: ['Drink coffee', 'Make money', 'Charity campaigns'],
 		icon: 'fa-coffee'
 	},
 	data2: {
-		title: 'Simple Worker',
-		date: 'Don\'t remember exactly',
-		summary: 'Counting, working, boring:',
+		title: 'Company Inc.',
+		date: '02/2016 - 02/2019',
+		summary: 'Simple Worker',
 		stack: true,
 		skills: [
 			{
-				stack: 'MYSQL',
-				icon: 'https://cdn.icon-icons.com/icons2/1495/PNG/512/mysqlworkbench_102952.png'
+				stack: 'Hard work',
+				icon: 'https://cdn4.iconfinder.com/data/icons/business-businessman-employee-workers/239/employee-life-style-009-512.png'
 			},
 			{
-				stack: 'JavaScript',
-				icon: 'https://cdn.iconscout.com/icon/free/png-256/javascript-16-458317.png'
+				stack: 'Coffee',
+				icon: 'https://cdn3.iconfinder.com/data/icons/coffee-icons/64/Cup_of_Coffee-512.png'
 			}
 		],
-		duties: ['make coffee', 'drink money'],
+		duties: ['Make coffee', 'Drink money', 'Hard working'],
 		icon: 'fa-walking'
+	},
+	data3: {
+		title: 'University of Lambada',
+		date: '10/2010 - 06/2015',
+		summary: 'Student:',
+		stack: true,
+		skills: [
+			{
+				stack: 'Learning',
+				icon: 'https://cdn.iconscout.com/icon/premium/png-256-thumb/machine-learning-17-910668.png'
+			}
+		],
+		duties: ['Learning new things', 'Going on exams', 'Hard party'],
+		icon: 'fa-glass-cheers'
 	}
 }
