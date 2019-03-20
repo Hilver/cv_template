2b0fc2d2eb67a6d40f621a78862403251538006a
Pawel <peczkowskip@gmail.com>
2019-03-20 15:47:59 +0100

    propably finished, TODO: checking errors, save as pdf

--- src/data/passion-data.js
+++ src/data/passion-data.js
@@ -1,28 +1,33 @@
 const passionData = [
 	{
 		icon: 'fa-grin-hearts',
-		text: 'Siatkówka, jazda na rowerze, gry logiczne (szachy, othello), nowe technologie, kuchnia azjatycka, dzikie plaże',
-		textClass: ['body-1']
+		text: 'Po pracy:',
+		textClass: 'body-2'
+	},
+	{
+		icon: 'fa-nothing',
+		text: 'Siatkówka, jazda na rowerze, gry logiczne (szachy, othello), kuchnia azjatycka, nowe technologie',
+		textClass: 'body-1'
 	},
 	{
 		icon: 'fa-grin-stars',
 		text: 'Czego chciałbym się nauczyć/rozwijać:',
-		textClass: ['body-2']
+		textClass: 'body-2'
 	},
 	{
 		icon: 'fa-nothing',
-		text: 'Vue, React, Node, Python, architektura kodu, programowanie funkcyjne',
-		textClass: ['body-1']
+		text: 'Vue/React, Node, Python, architektura oprogramowania, programowanie funkcyjne, machine learning',
+		textClass: 'body-1'
 	},
 	{
 		icon: 'fa-tired',
-		text: 'W jakiej technologii nie chciałbym się znaleźć:',
-		textClass: ['body-2']
+		text: 'Nie do końca przemawia do mnie:',
+		textClass: 'body-2'
 	},
 	{
 		icon: 'fa-nothing',
 		text: 'Angular, Java, PHP',
-		textClass: ['body-1']
+		textClass: 'body-1'
 	}
 ]
 
